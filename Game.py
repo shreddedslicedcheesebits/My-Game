d74a723c92aa0372406ced424547b6656e8bc2df
shreddedslicedcheesebites <brandon.staus@gmail.com>
2023-03-13 11:58:53 -0600

    added opening file

--- Game.py
+++ Game.py
@@ -1,38 +1,5 @@
+import opening.py
 #Items
 
 #starting room
-
-print("""
-Hello mortal,
-Welcome to your eternal suffering.
-
-Type any key to continue""")
-opening = ""
-while opening == "":
-	opening = input("")
-print("""
-You've wasted you're life always wanting
-more and more.
-
-Never stopping to consider, 
-look at what I've already achieved!
-
-Type any key to continue""")
-opening = ""
-while opening == "":
-	opening = input("")
-print("""
-You've pushed away everyone else
-who've helped you get there.
-Such a shame the way you had to go.
-
-Press any key to continue""")
-opening = ""
-while opening == "":
-	opening = input("")
-print("""
-So, now you can have it all!
-Forever.
-All the fancy decorations and food,
-in this mansion you will spend eternity
-alone.""")
+opening()
