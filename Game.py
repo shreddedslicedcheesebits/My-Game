3291025fb2d5c2230f561189b55dc25016c8e577
shreddedslicedcheesebites <brandon.staus@gmail.com>
2023-03-15 13:19:01 -0600

    trying to add 2

--- Game.py
+++ Game.py
@@ -17,5 +17,14 @@ while c != "q":
 {"Walk Right  d":^20}
 {"Surrender  q":^20}
 \nType Movement\n""")
+	if c == 2:
+		floor = input(f"""
+{"--Select a floor--":^20}
+{"Basement  0":^20}
+{"First floor  1":^20}
+{"Second Floor  2":^20}
+{"Third Floor  3":^20}
+
+{"Floor":<}""")
 #starting room
 floor_map(1)
