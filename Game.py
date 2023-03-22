1d954d643ff1788f6a6e2a1975b3cdb902a73718
shreddedslicedcheesebites <brandon.staus@gmail.com>
2023-03-22 13:07:52 -0600

    added show current room

--- Game.py
+++ Game.py
@@ -9,15 +9,16 @@ while c != "q":
 	c = input(f"""
 {"|-------Menu-------|":^20}
 {"View Inventory  1":^20}
-{"Show Floor maps  2":^20}
-{"Interact  3":^20}
+{"View Current room  2":^20}
+{"Show Floor maps  3":^20}
+{"Interact  4":^20}
 {"Walk forward  w":^20}
 {"Walk Backward  s":^20}
 {"Walk Left  a":^20}
 {"Walk Right  d":^20}
 {"Surrender  q":^20}
 \nType Movement\n""")
-	if c == "2":
+	if c == "3":
 		floor = int(input(f"""
 {"--Select a floor--":^20}
 {"Basement  0":^20}
